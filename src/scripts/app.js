6b6e35656b316395718d7080c63e7bee4a337370
Vasil <vasil97@yahoo.com>
2017-08-07 15:40:48 +0300

    IE and Edge fixes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,24 @@
 import {mainMenu} from './src/main-menu-dropdown';
 import {mobileMenu} from './src/mobile-menu';
 import {} from './src/slider';
+const $ = jQuery;
+
+$(document).ready(function () {
+    mainMenu('.menu-parent');
+    mobileMenu();
+
+    let isIE = () => {
+        return navigator.appName == 'Microsoft Internet Explorer' || (navigator.appName == "Netscape" && navigator.appVersion.indexOf('Edge') > -1);
+    };
 
 
-mainMenu('.menu-parent');
-mobileMenu();
+    // IE detection
+    if (isIE()) {
+        $('body').addClass('IE');
+    }
 
-
+    //Edge detection
+    if (/MSIE (\d+\.\d+);/.test(navigator.userAgent) || navigator.userAgent.indexOf("Trident/") > -1 ){
+        $('body').addClass('IE');
+    }
+});
--- src/scripts/src/slider.js
+++ src/scripts/src/slider.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
    $('.slider-paragraph__content').slick({
-       lazyLoad: 'ondemand',
        dots: true,
        infinite: true,
        speed: 300,
        slidesToShow: 1,
-       adaptiveHeight: true,
        arrows: false
    });
 });
